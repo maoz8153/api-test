cc6273caecbde357e40edaad2c9a1b8c96063bea
Maoz <maoz8153@gmail.com>
2020-10-10 22:25:19 +0300

    update readme and files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # api-test
+1. npm install
+
+to run localy with docker:
+    1.docker-compose  up --build -d
+
+
+to run on server ec2-18-184-130-111.eu-central-1.compute.amazonaws.com:
+    1. git clone https://github.com/maoz8153/api-test.git
+    2. npm install
+    3. npm run prod1
+
+to run on server ec2-18-184-17-86.eu-central-1.compute.amazonaws.com:
+    1. git clone https://github.com/maoz8153/api-test.git
+    2. npm install
+    3. npm run prod2
--- docker/server-mode.sh
+++ docker/server-mode.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-version=$(grep -oP '^VERSION=\K.*' ./.env)
-echo $version
-data=$(curl -u ) 
-buildNumber=$(sed -ne '/buildNumber/{s/.*<buildNumber>\(.*\)<\/buildNumber>.*/\1/p;q;}' <<< "$data")
-timestamp=$(sed -ne '/timestamp/{s/.*<timestamp>\(.*\)<\/timestamp>.*/\1/p;q;}' <<< "$data")
-echo $timestamp
-echo $buildNumber
-curl -u  -o fgpp-bff.tar.gz  -k
+server=$(grep -oP '^SERVER1=\K.*' ../config/server.env)
+echo $server
+data=$(curl -u $server/api/resource/mode) 
+slave=$(sed -ne '/SLAVE/{s/.*<SLAVE>\(.*\)<\/SLAVE>.*/\1/p;q;}' <<< "$data")
+master=$(sed -ne '/MASTER/{s/.*<MASTER>\(.*\)<\/MASTER>.*/\1/p;q;}' <<< "$data")
